61299c6c0c1889c375c903e1a80be25f00537d07
Siddharth Babu <21SBabu@heckgrammar.co.uk>
2025-01-16 12:20:21 +0000

    MergeSortRecursive

--- Program.cs
+++ Program.cs
@@ -6,17 +6,61 @@ namespace compare_algorithm
     {
         static void Main(string[] args)
         {
+            int userAnswer = 0;
             Random n = new Random();
             Stopwatch sw = new Stopwatch();
+            Console.WriteLine("What size of array");
+            int Size = Convert.ToInt32(Console.ReadLine());
+            int[] array = CreateArray(Size, n);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            menu(array);
 
 
         }
         static int[] CreateArray(int size, Random r)
         {
-            
+            int[] array = new int[size];
+            for (int k  = 0; k < array.Length; k++)
+            {
+                array[k] = r.Next(0, 100);
+            }
+            Console.WriteLine("Array from 1 to 100 created");
+            return array;
         }
-        static void menu()
+        static void menu(int[] array)
         {
+            Console.WriteLine("1. Linear search");
+            Console.WriteLine("2. Binary search");
+            Console.WriteLine("3. Merge sort");
+            Console.WriteLine("4. Bubble sort");
+            Console.WriteLine("5. Quit");
+            Console.WriteLine("Write the number");
+            int userAnswer = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(userAnswer);
+            if (userAnswer == 1)
+            {
+                Console.WriteLine("What would you like to find");
+                int numToFind = Convert.ToInt32(Console.ReadLine());
+                LinearSearch(array, numToFind);
+            }
+            else if (userAnswer == 2)
+            {
+                Console.WriteLine("What would you like to find");
+                int numToFind = Convert.ToInt32(Console.ReadLine());
+                LinearSearch(array, numToFind);
+            }
+            else if (userAnswer == 3)
+            {
+                BubbleSort(array);
+            }
+            else if(userAnswer == 4)
+            {
+                MergeSortRecursive
+            }
+
 
         }
         
